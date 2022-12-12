eae988264ec886e11b422058d419f883283cd1f8
Stephengera <01geraldstephen@gmail.com>
2022-12-11 16:30:00 -0800

    fifth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - print a c program using printf
- *
- * Return; 0
+ *  * main - print a string
+ *   * Description: use printf
+ *    * Return: 0
  */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
